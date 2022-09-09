fef3c3775e7fa47988ed6d5625e0aa7e93bf31c5
Chantell <chantell@Chantells-MacBook-Pro.local>
2022-09-10 00:02:55 +1000

    modified files in dataset folder to reflect lag analysis. saved csv file and graphical plot generated from analysis

--- dataset_content/monkeypox.py
+++ dataset_content/monkeypox.py
@@ -10,6 +10,7 @@
 #############################################################################################################################################
 
 # IMPORTS ALL MODULES
+
 import time
 import numpy as np
 import pandas as pd
@@ -42,9 +43,15 @@ def load_data():
     monkeypox_df = monkeypox_df[monkeypox_df.Status != 'omit_error']
     return monkeypox_df
 
+# FUNCTION TO AGGREGATE SOURCES
+def agg_sources(df):
+    out_df = df.copy()
+    return out_df
+
 # FUNCTION TO WRANGLE THE DATA
 def wrangle(df):
     mod_df = df.copy()
+    mod_df = agg_sources(mod_df)
     return mod_df
 
 # FUNCTION TO ANALYSE THE SYNTOMS COMMONLY EXPERIENCED BY CASES WHICH HAVE BEEN CLASSIFIED
@@ -56,3 +63,37 @@ def symptoms(df):
 def sql_ready(df, num_tabs, tab_label):
 
     return 
+
+#### FUNCTIONS FOR AGGREGATION
+
+# FUNCTION USED TO AGGREGATE COLUMNS BASED ON THE AUSTRALIAN STATES
+# Imports datetime modules for this function
+from datetime import datetime as dt
+from datetime import timedelta as td
+
+# Function used to fill in all missing dates with 0's until current date at given locations
+def date_filler(df):
+    locations = df.Location.unique()
+    current_date = (dt.today() + td(days=1)).date()
+    min_date = df.Date_confirmation.min()
+    for place in locations:
+        date_stamp = min_date
+        subdf = df.loc[df['Location'] == place]
+        subdf_add = {'Location': [], 'Date_confirmation': [], 'cases': []}
+        while date_stamp <= current_date:
+            if date_stamp not in list(subdf.Date_confirmation):
+                subdf_add['Location'].append(place)
+                subdf_add['Date_confirmation'].append(date_stamp)
+                subdf_add['cases'].append(0)
+            else:
+                subdf_add['Location'].append(place)
+                subdf_add['Date_confirmation'].append(date_stamp)
+                subdf_add['cases'].append(subdf.loc[subdf['Date_confirmation'] == date_stamp, 'cases'].values[0])
+            date_stamp = date_stamp + td(days=1)
+        #subdf.append({'Location': place, 'Date_confirmation': date_stamp, 'cases': 0}, ignore_index=True)
+        #subdf = pd.concat([subdf, pd.DataFrame(subdf_add)], ignore_index= True, axis=0)
+        if place == locations[0]:
+            df_out = pd.DataFrame(subdf_add)
+        else: 
+            df_out = pd.concat([df_out, pd.DataFrame(subdf_add)], ignore_index=True, axis=0)
+    return df_out
