905953bf829b2e8fbcf1ff3a4e51ca30547bce6a
kinoshita197083 <83145802+kinoshita197083@users.noreply.github.com>
2022-09-19 12:39:43 +1000

    fix

--- src/components/home_section.js
+++ src/components/home_section.js
@@ -23,28 +23,28 @@ export function HomeSection() {
                     <div>
                         <img src="https://anythingfrenkie.s3.ap-southeast-2.amazonaws.com/bw-tattoo.png" alt="" />
                         <h1>Tatto</h1>
-                        <p style={{fontSize:'1.5rem', 'font-style': 'italic', 'font-weight': 'normal'}}>Artists</p>
+                        <p style={{ fontSize: '1.5rem', 'font-style': 'italic', 'font-weight': 'normal' }}>Artists</p>
                     </div>
                 </li>
                 <li>
                     <div>
                         <img src="https://anythingfrenkie.s3.ap-southeast-2.amazonaws.com/bw-spa.png" alt="" />
                         <h1>Massage</h1>
-                        <p style={{fontSize:'1.5rem', 'font-style': 'italic', 'font-weight': 'normal'}}>Therapists</p>
+                        <p style={{ fontSize: '1.5rem', 'font-style': 'italic', 'font-weight': 'normal' }}>Therapists</p>
                     </div>
                 </li>
                 <li>
                     <div>
                         <img src="https://anythingfrenkie.s3.ap-southeast-2.amazonaws.com/bw-boxing.png" alt="" />
                         <h1>Personal</h1>
-                        <p style={{fontSize:'1.5rem', 'font-style': 'italic', 'font-weight': 'normal'}}>Trainers</p>
+                        <p style={{ fontSize: '1.5rem', 'font-style': 'italic', 'font-weight': 'normal' }}>Trainers</p>
                     </div>
                 </li>
                 <li>
                     <div>
                         <img src="https://anythingfrenkie.s3.ap-southeast-2.amazonaws.com/bw-carer.png" alt="Personal Carers" />
-                        <h1>Peresonal</h1>
-                        <p style={{fontSize:'1.5rem', 'font-style': 'italic', 'font-weight': 'normal'}}>Carers</p>
+                        <h1>Personal</h1>
+                        <p style={{ fontSize: '1.5rem', 'font-style': 'italic', 'font-weight': 'normal' }}>Carers</p>
                     </div>
                 </li>
                 <li class="pusher"></li>
